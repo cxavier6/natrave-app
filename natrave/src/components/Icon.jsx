41e6c19ba2060f2a6351c617630bb06f52f8ad0c
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-10-10 20:47:48 -0300

    feat: adiciona ícones e altera a cor

--- natrave/src/components/Icon.jsx
+++ natrave/src/components/Icon.jsx
@@ -1,7 +1,14 @@
 import { ReactComponent as back} from '../assets/imgs/icones/back.svg'
+import { ReactComponent as arrowLeft} from '../assets/imgs/icones/arrow-left.svg'
+import { ReactComponent as arrowRight} from '../assets/imgs/icones/arrow-right.svg'
+import { ReactComponent as profile} from '../assets/imgs/icones/profile.svg'
 
 const icons = {
-    back
+    back, 
+    arrowLeft,
+    arrowRight,
+    back,
+    profile
 }
 
 export const Icon = ({ name, ...props }) => {
