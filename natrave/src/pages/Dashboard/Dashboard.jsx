bf0c275127c3ed8a0fa9bb2a6859a027f951f77f
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-10-11 21:45:46 -0300

    feat: adiciona seção de content da página

--- natrave/src/pages/Dashboard/Dashboard.jsx
+++ natrave/src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,7 @@
-import logo from '../../assets/imgs/logo/logo-fundo-vermelho.svg'
 import { Icon } from '../../components/Icon'
+import logo from '../../assets/imgs/logo/logo-fundo-vermelho.svg'
+import sui from '../../assets/imgs/bandeiras/sui.png'
+import cam from '../../assets/imgs/bandeiras/cam.png'
 
 export const Dashboard = () => {
   return (
@@ -22,8 +24,22 @@ export const Dashboard = () => {
                 </div>
             </section>
 
-            <section id='content'>
+            <section id='content' className='container max-w-3xl p-4 m-auto'>
+                <div className='rounded-xl border border-gray-300 text-center p-4 space-y-4'>
+                    <span className='text-sm md:text-base text-gray-700 font-bold'>7:00</span>
+                    
+                    <div className='flex space-x-4 justify-center items-center'>
+                        <span className='uppercase'>sui</span>
+                        <img src={sui}/>
+                        
+                        <input max={10} type="number" className='bg-red-300/[0.2] w-[55px] h-[55px] text-red-700 text-xl text-center'/>
+                        <span className='text-red-500 text-bold'>X</span>
+                        <input max={10} type="number" className='bg-red-300/[0.2] w-[55px] h-[55px] font-red-700 text-xl text-center'/>
 
+                        <img src={cam}/>
+                        <span className='uppercase'>cam</span>
+                    </div>
+                </div>
             </section>
         </main>
     </div>
